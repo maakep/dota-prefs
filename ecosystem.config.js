62fa98078488306f9d2c8f830fae63d4993c6e67
Maakep <schon.fredrik@gmail.com>
2022-01-16 19:52:27 +0100

    Fix pm2 setup

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,9 +2,9 @@ module.exports = {
   apps: [
     {
       name: "dota-prefs",
-      script: "./dist/index.js",
+      script: "npm run startup",
       env_hook: {
-        command: "git pull && npm i && pm2 restart dota-prefs",
+        command: "git pull && pm2 restart dota-prefs",
         cwd: "/home/pi/projects/dota-prefs",
       },
     },
